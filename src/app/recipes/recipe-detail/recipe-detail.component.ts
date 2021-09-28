0042526a75609938d10f732be0453c87c0469f4a
jlozanol <jlozanol@vectoritcgroup.com>
2021-09-28 17:14:20 +0200

    configuring route parameters

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,23 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../../models/recipe.model';
 import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
 
-  @Input() recipe: Recipe;
-
-  constructor(private recipeSercive: RecipeService) { }
+  constructor(
+    private recipeSercive: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    // const id = this.route.snapshot.params['id'];
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeSercive.getRecipe(this.id);
+      console.log('entra');
+    });
   }
 
   onAddToShoppingList() {
     this.recipeSercive.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-  
 }
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,23 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../../models/recipe.model';
-import { RecipeService } from '../../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
-
   @Input() recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
-
-  ngOnInit(): void {
-  }
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
-
+  ngOnInit(): void {}
 }
